770443a9e8887d7e79d5c28d4936749ba4d5dc1e
srinivasreddyramaram <rsrinivasreddy05@gmail.com>
2020-01-24 11:08:06 +0530

    Add sessionStorage to check if user is logged in or not

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,17 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log("before " + this.isUserLoggedIn());
     if (username === 'cnu' && password === 'pass123') {
+      sessionStorage.setItem('loggedInUser', username);
+      console.log("after " + this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('loggedInUser');
+    return !(user == null);
   }
 }
