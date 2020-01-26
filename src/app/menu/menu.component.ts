79803a266cf4bc5963a4768b728dba7e8339654f
srinivasreddyramaram <rsrinivasreddy05@gmail.com>
2020-01-26 15:41:33 +0530

    Inject authentication service into menu component, use ngIf directive to enable/disable links

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  //loggedInUser: boolean = false;
+
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
+    
+    //this.loggedInUser = this.hardcodedAuthenticationService.isUserLoggedIn();
   }
 
 }
